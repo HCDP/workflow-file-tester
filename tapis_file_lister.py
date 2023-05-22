be4811ed71cb6c7c137cfd0e5ddb803ae2b3bef9
Sean Cleveland <sean.b.cleveland@gmail.com>
2023-05-22 12:48:55 -0600

    Update tapis_file_lister.py
    remove extra config loading code and add some instructions for further development

--- tapis_file_lister.py
+++ tapis_file_lister.py
@@ -1,13 +1,12 @@
 import json
 from tapipy.tapis import Tapis
 
-# Load data from json files
+# Load config data from json files
 with open('config.json', 'r') as configfile:
     configData = json.load(configfile)
-with open('system_config.json', 'r') as systemconfigfile:
-    storage_system = json.load(systemconfigfile)
-with open('cred_tmp.json', 'r') as credentialsfile:
-    credentials = json.load(credentialsfile)
+    
+# Load the upload.json config file 
+# ADD NEW CODE HERE
 
 # Create tapis instance
 t = Tapis(base_url=configData["tapis_instance"]["base_url"], username=configData["tapis_instance"]["username"], password=configData["tapis_instance"]["password"], tenant_id=configData["tapis_instance"]["tenant_id"])
@@ -15,8 +14,15 @@ t = Tapis(base_url=configData["tapis_instance"]["base_url"], username=configData
 # Generate access token
 t.get_tokens()
 
+# Read the upload field and loop through each entry
+# for each entry construct a new path for a file by combining the remote_path field with the rename field with a back slash between them
+# replace each occurence of %y, %m, %d with the correpsonding current year, month or day
+# then using the Tapis Files API check the existence of the newly created filepathed filename and that it's size is > 0
+
 # Access the file system
-tapis_files = t.files.listFiles(systemId=configData["tapis_user_credentials"]["systemId"], path=configData["tapis_user_credentials"]["list_file_path"])
+tapis_files = t.files.listFiles(systemId="SYSTEM_ID_GOES_HERE", path="NEW_PATH_GOES_HERE")
 for ts in tapis_files:
     if ts.size > 0 and ts.type == "file":
         print("this file exists with path: " + ts.path + " size: " + str(ts.size))
+     
+# If the filepath doesn't exist or the file size is 0 generate an email notification
